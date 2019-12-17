2676e7392fcb3c69f3ccc2dec3d9342ff8b819ba
Zico13 <45403895+Zico13@users.noreply.github.com>
2019-12-17 10:35:25 +0300

    Update telbot.py

--- telbot.py
+++ telbot.py
@@ -4,7 +4,7 @@ import cv2
 
 artur_id = "239289123"
 sofi_id = "870800205"
-BOT_TOKEN = "888729683:AAGYwRwy2xrDuZnD8PtD9-v0S2aDhBh9r-g"
+
 telebot.apihelper.proxy = {'https': "63.249.67.70:53281"}
 bot = telebot.TeleBot(BOT_TOKEN)
 print(bot.get_me())
