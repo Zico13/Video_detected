7b9f7ecbeffaec5aeba1981a0528ef9e8f4b5b33
Zico13 <45403895+Zico13@users.noreply.github.com>
2019-12-25 10:04:23 +0300

    Update telbot.py

--- telbot.py
+++ telbot.py
@@ -2,8 +2,7 @@ import datetime
 import telebot
 import cv2
 
-artur_id = "239289123"
-sofi_id = "870800205"
+
 
 telebot.apihelper.proxy = {'https': "63.249.67.70:53281"}
 bot = telebot.TeleBot(BOT_TOKEN)
